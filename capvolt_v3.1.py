0d744ef110833e443f17e116f55436ccd2134653
LiamBird <43959073+LiamBird@users.noreply.github.com>
2019-03-29 15:37:10 +0000

    Add files via upload

---
+++ capvolt_v3.1.py
@@ -0,0 +1,7 @@
+from capvolt_fun import *
+
+path = r'C:\Users\llrb2\Dropbox\DATA\Capvolt\(1-0-2)(0-10)T'
+filename = r'cell4_rc'
+cycles_to_show = [1, 2, 10]
+
+capvolt_fun(path, filename, cycles_to_show)
