6902f9ff8e9f5e9c0c1722043865470747c12454
LiamBird <43959073+LiamBird@users.noreply.github.com>
2019-03-29 15:37:52 +0000

    Delete capvolt_v3.1.py

--- capvolt_v3.1.py
+++
@@ -1,7 +0,0 @@
-from capvolt_fun import *
-
-path = r'C:\Users\llrb2\Dropbox\DATA\Capvolt\(1-0-2)(0-10)T'
-filename = r'cell4_rc'
-cycles_to_show = [1, 2, 10]
-
-capvolt_fun(path, filename, cycles_to_show)
